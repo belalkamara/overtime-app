6477d9d55b8fbb8f80c0076f13101d454f50cdbb
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2017-12-25 13:30:27 -0600

    Implemented the full name method to display on posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
+  before do
       @user = User.create(email: "test@test.com", password: "testtest", password_confirmation: "testtest", first_name: "Billy", last_name: "Goat")
     end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -14,6 +14,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name methods" do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq("GOAT, BILLY")
     end
   end
 end

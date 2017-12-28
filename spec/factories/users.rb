162ab3a1f425be6d5e8f043ed5d37266491a8cdc
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2017-12-27 20:18:27 -0600

    Added FactoryGirl and refactored code in the features and model dir

---
+++ spec/factories/users.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  sequence :email do |n|
+    "test#{n}@example.com"
+  end
+
+  factory :user do
+    first_name 'Billy'
+    last_name 'Goat'
+    email { generate :email }
+    password "testtest"
+    password_confirmation "testtest"
+  end
+
+  factory :admin_user, class: "AdminUser" do
+    first_name 'Admin'
+    last_name 'User'
+    email { generate :email }
+    password "testtest"
+    password_confirmation "testtest"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-      @user = User.create(email: "test@test.com", password: "testtest", password_confirmation: "testtest", first_name: "Billy", last_name: "Goat")
+      @user = FactoryGirl.create(:user)
     end
 
   describe "creation" do

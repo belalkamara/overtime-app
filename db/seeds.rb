b080d5ed6c4c9fef3aaee624ee29cb319a03897e
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2018-01-05 21:06:33 -0600

    Added radio buttons to edit status in entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 User Created"
 
+AdminUser.create(email: "admin@test.com", password: "testtest", password_confirmation: "testtest", first_name: "Admin", last_name: "User")
+
+puts "1 Admin User Created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end

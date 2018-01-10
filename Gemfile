8aa1bfc25c760eabbd6459ae73179e9f842a1677
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2018-01-09 20:04:45 -0600

    Added hours requested to post models

--- Gemfile
+++ Gemfile
@@ -41,3 +41,4 @@ gem 'administrate', '~> 0.8.1'
 gem 'bourbon'
 gem 'pundit', '~> 1.1'
 
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101235131) do
+ActiveRecord::Schema.define(version: 20180110005733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180101235131) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "status", default: 0
+    t.decimal "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

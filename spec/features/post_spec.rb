56b8cea66fef32e99a03186d433367ec44a07c33
Belal Kamara <belalkamara@gmail.com>
2018-02-09 20:33:16 -0600

    Fixed syntax that woun't display the nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -44,7 +44,7 @@ describe 'navigate' do
   describe'new' do
     it 'has a link from the homepage' do
       employee = Employee.create(first_name: 'Employee', last_name: 'YA', email: "employee@test.com", password: "testtest", password_confirmation: "testtest", phone: "9018304494")
-      login_as(user, :scope => :user)
+      login_as(employee, :scope => :user)
       visit root_path
 
       click_link("new_post_from_nav")
